841e3c33e3d033ee65c1a5fde8ce6183abd8c8eb
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-07-17 21:03:36 +0900

    add

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # kukumail.js
 
 JavaScript Library for "m.kuku.lu"
+Created by [@amex2189](https://twitter.com/amex2189)
 
 注意(JP): SESSIONは何度も生成すると弾かれます。弾かれた場合はWebからcf_clearnceを取得するか、ipを変更してください。出来るだけ一度生成したら使いまわしてください。
+
+## Usage
+Coming soon...
--- class/kukumail.ts
+++ class/kukumail.ts
@@ -1,9 +1,11 @@
 import { createAccount } from "../lib/operation/createAccount";
+import { getCsrfToken } from "../lib/utils/getCsrfToken";
 
 export class Kukumail {
 	initlized = false;
 	sessionHash?: string;
 	csrfToken?: string;
+	csrfSubToken?: string;
 	cfClearance?: string;
 
 	constructor(sessionHash?: string, csrfToken?: string, cfClearance?: string) {
@@ -15,7 +17,12 @@ export class Kukumail {
 			if (cfClearance) {
 				this.cfClearance = cfClearance;
 			}
+			this.updateCsrfToken();
 		}
+
+		setInterval(async () => {
+			await this.updateCsrfToken();
+		}, 24 * 60 * 60 * 1000);
 	}
 
 	buildBaseCookie() {
@@ -36,11 +43,36 @@ export class Kukumail {
 		this.initlized = true;
 		this.sessionHash = result.data.session_hash;
 		this.csrfToken = result.data.csrf_token;
+		await this.updateCsrfToken();
+
+		return this;
+	}
+
+	async updateCsrfToken() {
+		this.guardNonInitlized();
+		const result = await getCsrfToken(this.sessionHash as string, this.csrfToken as string, this.buildBaseCookie());
+
+		if (result.type === "error") {
+			throw new Error(result.data);
+		}else {
+			this.csrfToken = result.data.csrf_token;
+			this.csrfSubToken = result.data.csrf_subtoken;
+		}
+
+		return this;
 	}
 
 	guardNonInitlized() {
 		if (!this.initlized) {
 			throw new Error("Kukumail is not initialized, call createAccount() first");
 		}
+	}
+
+	async waitForInitlized() {
+		while (!this.initlized || !this.csrfSubToken) {
+			await new Promise((resolve) => setTimeout(resolve, 1000));
+		}
+
+		return this;
 	}
 }
